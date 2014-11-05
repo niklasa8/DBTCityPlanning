4b7c84ca9b4a4c26a7642d983210c4a0ad0dcc33
Jonas Lindgren <joli0110@student.umu.se>
2014-11-05 15:21:27 +0100

    update

--- CPGUI/cursor_coord.m
+++ CPGUI/cursor_coord.m
@@ -1,8 +1,7 @@
-function cursor_coord(hObject,eventData,handles)
-handles=guidata(hObject);
+function coordinates = cursor_coord(hObject,eventData,handles)
+handles = guidata(hObject);
 coordinates = get(gca,'CurrentPoint');
-set (handles.text2,'string',{'Cursor Pixels',num2str(coordinates(1,1))...
-                               num2str(coordinates(1,2))})
-
+set(handles.text2,'string',{'Cursor Pixels',num2str(coordinates(1,1))...
+                               num2str(coordinates(1,2))});                        
 end
 
