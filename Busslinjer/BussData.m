38403354be0ce80b926d2b17e20af474f6221903
Niklas Andersson <nian0021@gmail.com>
2014-11-13 18:01:50 +0100

    återstår att läsa av fotnoter

--- Busslinjer/BussData.m
+++ Busslinjer/BussData.m
@@ -8,11 +8,11 @@ f = char(textFile);
 
 % Läs in riktning och fotnot
 dir = str2num(f(index(1)+2:index(2)-1))
-footnote = f(index(2)+1:end-4)
+footnote = upper(f(index(2)+1:end-4))
 
 % Loopa över alla senare
-file_idTXT = fopen(char(textFile(1)));
-file_idXML = fopen(char(xmlFile(1)));
+file_idTXT = fopen(char(textFile(1)),'r','n','UTF-8');
+file_idXML = fopen(char(xmlFile(1)),'r','n','UTF-8');
 
 % Läs in busshållplatser från fil
 stopnr=1;
@@ -28,15 +28,16 @@ A = '';
 corr_dir = 0;
 
 % Läs in data från xmlfil
+k=1;%index för monThr
 while (~feof(file_idXML)) & (isempty(strfind(A,'</Direction>')))
     A = fgets(file_idXML);
     
     % Placera filpekaren enligt riktning(direction:1,2)
     if corr_dir==0 && dir == 2
         while isempty(strfind(A,'</Direction>'))
-            A = fgets(file_idXML)
+            A = fgets(file_idXML);
         end
-        corr_dir = 1;
+        corr_dir = 1
         A = fgets(file_idXML);
     end
     
@@ -45,10 +46,10 @@ while (~feof(file_idXML)) & (isempty(strfind(A,'</Direction>')))
          while isempty(strfind(A,'</Cols>')) %Läser in alla busshållplatser som linjen stannar vid.
 
             if strcmp(A(9:10),'<C')
-                col_pos = str2double(A(19))
+                col_pos = str2double(A(19));
                 A = fgets(file_idXML);
                 end_index = strfind(A,'<');
-                bus_stop=A(31:end_index(2)-1)
+                bus_stop=A(31:end_index(2)-1);
                 
                 % Lagra pos-nr från xmlfil för hållplatsen
                 for i=1:stopnr-1
@@ -61,15 +62,47 @@ while (~feof(file_idXML)) & (isempty(strfind(A,'</Direction>')))
             A=fgets(file_idXML);
          end
     end
-    
-    if ~(isempty(strfind(A, '<DayTypes>')))
-        if ~(isempty(strfind(A, '<DayType>')))
-            
-            if ~(isempty(strfind(A, '<DayName>')))
-                
+   % while~isempty(strfind(A,'</DayTypes>'))
+    %    A=fgets(file_idXML)
+                   
+            if ~(isempty(strfind(A, '<DayType>')))
+                A=fgets(file_idXML);
+                end_index = findstr(A,'<');
+                day_name = A(20:end_index(2)-1); %Sparar vilka dagar denna resa gäller
+                %A=fgets(file_idXML);
+                if ~isempty(strfind(day_name,'måndag-fredag'))
+                    A=fgets(file_idXML);
+                    while isempty(strfind(A,'</Rows>')) %Läser in avgångstider för resan.
+                        
+                      if ~isempty(strfind(A,'<Time pos="'))
+                            row_pos = 2double(A(19));
+                            A=fgets(file_idXML);
+                            end_index = findstr(A,'<');
+                            dept_time_str = A(39:end_index(2)-1)
+                            monThr(row_pos+1).dep=dept_time_str;
+                            A=fgets(file_idXML);
+                            if ~isempty(strfind(A,'<TimeFootnoteId>'))
+                                A
+                            end   
+                            k=k+1;
+                      end
+                      
+                        
+                      if ~isempty(strfind(A,strcat('<Time pos="',num2str(stop(1).pos))))
+                        A=fgets(file_idXML);
+                        end_index = findstr(A,'<');
+                        dept_time_str = A(39:end_index(2)-1)
+                        monThr(k).dep=dept_time_str;
+                        k=k+1;
+                      end
+                      %else if 
+                      A=fgets(file_idXML);
+                    end
+                end
+                if ~isempty(strfind(day_name,'fredag'))
+                    %fortsätt
+                end
             end
-
-            
-        end
-    end
+                
+    A=fgets(file_idXML);
 end
