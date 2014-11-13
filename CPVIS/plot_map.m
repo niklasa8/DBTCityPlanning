0e32312d68639979300e26a29ad3a87c438d028f
hs90 <hampusira@gmail.com>
2014-11-13 23:25:45 +0100

    geometri
    Den här kartan är preliminär. Gjort kartan skalenlig. Ska lägga till
    områden. Kompatibel med MATLAB R2012b. Använd plot_map.m annars.

--- CPVIS/plot_map.m
+++ CPVIS/plot_map.m
@@ -10,7 +10,7 @@ mapshow(umenatur1,'facecolor',[0.6 1 0.6],'edgecolor',[0.6 1 0.6]);
 title('Umeå med omnejd')
 xlabel('° E')
 ylabel('° N')
-axis equal
+daspect([1 0.44 1])
 axis([20.1129 20.4617 63.7845 63.8771])
 hold on
 
@@ -21,7 +21,7 @@ mapshow(umeakermark,'facecolor',[1 0.85 0.55],'edgecolor',[1 0.85 0.55]);
 mapshow(umefstght,'facecolor',[0.9 0.8 0.9],'edgecolor',[0.9 0.8 0.9]);
 toc
 
-h_p = plot(20.2,63.8,'.w');
+h_p = plot(20.2,63.8,'.w','markersize',1);
 h_l = legend(h_p,'© OpenStreetMaps bidragsgivare');
 h_l = legend(h_l,'location','southeast');
 h_l = legend(h_l,'boxoff');
