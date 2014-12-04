89024bc750b727feabc85d088863d073f3c2a9fa
j0hnne <johnbergman.se@gmail.com>
2014-12-04 11:42:41 +0100

    arb på matris-skript, ändrat Paradisgränd
    Kontrollerat och Paradisgränd för linje 1,73,81 och ändrat där det
    behövdes.

--- Busslinjer/create_arr_dep.m
+++ Busslinjer/create_arr_dep.m
@@ -1,17 +1,16 @@
 clear all
 clc
 
-datafiles={};
-k=1;
-BussData=dir('BussData')
-% bs_map = containers.Map('KeyType','char','ValueType','int32');
-
-num_stops = 180;
-A = cell(num_stops);
-
-TimeToMin=@(t)mod(t,1)*100+floor(t)*60; %t=7.56-> 7 h 56 min omvandlar detta till antal minuter från 00:00
 
 
+% Function
+TimeToMin = @(t) mod(t,1)*100+floor(t)*60; %t=7.56-> 7 h 56 min omvandlar detta till antal minuter från 00:00
+
+
+% Read filenames
+datafiles = {};
+k = 1;
+BussData = dir('BussData')
 
 for i=3:max(size(BussData))
     directory=['BussData/' BussData(i).name '/'];
@@ -22,36 +21,79 @@ for i=3:max(size(BussData))
     end
 end
 
-for file=1%max(size(datafiles))
-    load(char(datafiles(file)));
+% Get number of busstops
+Names={};
+IDs={};
+busMap = containers.Map('KeyType','double','ValueType','int32');
+num_stops = 0;
+for i=1:length(datafiles)
+    data=load(datafiles{i});
+    data=data.table;
+    for j=1:length(data)
+        name=data(j).name;
+        id=data(j).id;
+        
+        if ~isKey(busMap,id)
+           busMap(id) = num_stops+1;
+           num_stops = num_stops+1;
+        end       
+        
+        if ~ismember(id, IDs)
+            IDs=[IDs id];
+        end
+        
+        if ~ismember(name, Names);
+            Names=[Names name];
+        end
+    end
+end
 
-   
+n = length(IDs);  % Total number of busstops 
+
+Arr = cell(n);  % Arrival times
+Dep = cell(n);  % Departure times
+
+
+for file=1%max(size(datafiles))
+    load(char(datafiles(file)));   
+    
     
     for i=1:max(size(table))-1
+        
+        
         if isempty(table(i).waiting_time)
             table(i).waiting_time = 0;
         end
-
+        
+        % Handle roundoffs in waitingtime
         wait_time = round(table(i).waiting_time);
         if wait_time-table(i).waiting_time < 0
            table(i+1).waiting_time = abs(wait_time-table(i).waiting_time) + table(i+1).waiting_time
-
         elseif wait_time-table(i).waiting_time > 0
            table(i+1).waiting_time = table(i).waiting_time - abs(wait_time-table(i).waiting_time)
         end
         table(i).waiting_timeround = wait_time;
         
-        % Kontrollera vilka dagar som har avgångstider
+        
+        % For separate days
         if ~isempty(table(1).MT)
-%             TimeToMin(table(i+1).MT) - TimeToMin(table(i).MT) + round(table(i).waiting_time
-
+            dep = TimeToMin(table(i+1).MT) - TimeToMin(table(i).MT) + round(table(i).waiting_time);
+            arr = TimeToMin(table(i+1).MT);
         end
+        
         if isempty(table(1).F)
+           dep = TimeToMin(table(i+1).F) - TimeToMin(table(i).F) + round(table(i).waiting_time);
+           arr = TimeToMin(table(i+1).F);
         end
+        
         if isempty(table(1).L)
+           dep = TimeToMin(table(i+1).L) - TimeToMin(table(i).L) + round(table(i).waiting_time);
+           arr = TimeToMin(table(i+1).L);
         end
+        
         if isempty(table(1).S)
-            
+           dep = TimeToMin(table(i+1).S) - TimeToMin(table(i).S) + round(table(i).waiting_time);
+           arr = TimeToMin(table(i+1).S);
         end
         
     end
